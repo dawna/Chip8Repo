fc41c4af4e4c0c54464d543f9d80ab3336f3eb73
Adam Dawn <AdamD@magenic.com>
2016-07-14 15:59:22 -0500

    Added 2 new files

---
+++ Projects/Chip8Emulator/Chip8Emulator/Chip8DataModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8Emulator
+{
+    public class Chip8DataModel
+    {
+        //16 bit registers.
+        public int[] V { get; set; }
+        //32 bit register (usually stores memory addresses).
+        public int I { get; set; }
+        //? bit register (Delay Timer).
+        public int DT { get; set; }
+        //? bit register (Sound Timer).
+        public int ST { get; set; }
+        //16 bit program counter.
+        public int PC { get; set; }
+        //8 bit stack pointer.
+        public byte SP { get; set; }
+        //Stores 16 16-bit values.
+        public int[] Stack { get; set; }
+    }
+}
---
+++ Projects/Chip8Emulator/Chip8Emulator/OpcodeInstructions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8Emulator
+{
+    public class OpcodeInstructions
+    {
+        public static Action<Chip8DataModel> Jump = delegate(Chip8DataModel data)
+        {
+            //Do stuff
+        };
+    }
+}
