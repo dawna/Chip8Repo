3223e59967696c2b6a939a692fb3a63a1dede9f0
Adam Dawn <AdamD@magenic.com>
2016-07-14 15:06:06 -0500

    Added a bunch of files.  Got the framework down.

--- Projects/Chip8Emulator/Chip8Emulator/Chip8EmulatorMain.cs
+++ Projects/Chip8Emulator/Chip8Emulator/Chip8EmulatorMain.cs
@@ -12,18 +12,25 @@ namespace Chip8Emulator
         public static int[] ReadRomFromFile(string filePath)
         {
             var romData = new int[ConstantValues.TOTAL_MEM_STORAGE];
-            byte[] byteArray = File.ReadAllBytes(filePath);
 
-            if (byteArray.Length > ConstantValues.TOTAL_MEM_STORAGE)
+            try
             {
-                //Throw exception.
-            }
+                byte[] byteArray = File.ReadAllBytes(filePath);
 
-            int counter = 0;
-            for (int i = 0; i < byteArray.Length - 1; i+= 2)
+                if (byteArray.Length > ConstantValues.TOTAL_MEM_STORAGE)
+                {
+                    //Throw exception.
+                }
+
+                int counter = 0;
+                for (int i = 0; i < byteArray.Length - 1; i += 2)
+                {
+                    romData[counter] = (byteArray[i] << 8) | byteArray[i + 1];
+                    counter++;
+                }
+            }catch (Exception e)
             {
-                romData[counter] = (byteArray[i] << 8) | byteArray[i + 1];
-                counter++;
+                Console.WriteLine("Couldn't open file");
             }
 
             return romData;
@@ -31,7 +38,7 @@ namespace Chip8Emulator
 
         static void Main(string[] args)
         {
-            string filePath = @"c:\users\adam\documents\visual studio 2013\Projects\Chip8Emulator\Chip8Emulator\c8games\VERS";
+            string filePath = @"C:\Users\AdamD\Source\Repos\Chip8Repo\Projects\Chip8Emulator\Chip8Emulator\c8games\VERS";
 
             //Retrieves ROM and stores it.
             ReadRomFromFile(filePath);
---
+++ Projects/Chip8Emulator/Chip8Emulator/Chip8Interpreter.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8Emulator
+{
+    public class Chip8Interpreter
+    {
+        private readonly Renderer _renderer{ get; set; }
+        private readonly int[] _rom { get;  set; }
+        private readonly ROMType _romType { get;  set; }
+
+        private Chip8DataModel ConfigurationsModel { get; set; }
+         
+        public Chip8Interpreter(int[] _rom, ROMType romType)
+        {
+            this._rom = _rom;
+            this.InitializeInterpreter(romType);
+        }
+
+        public Chip8Interpreter(ROMType romType)
+        {
+            this.InitializeInterpreter(romType);
+        }
+
+        public void Reset()
+        {
+            this.InitializeInterpreter(this._romType);
+        }
+        
+        private void InitializeInterpreter(ROMType romType)
+        {
+            this._romType = romType;
+
+            //Default starting value for PC
+            var localPC = 0x200;
+            if (this._romType == ROMType.ETI660)
+            {
+                localPC = 0x600;
+            }
+
+            this.ConfigurationsModel = new Chip8DataModel
+            {
+                Stack = new int[0xF],
+                V = new int[0xF],
+                PC = localPC
+            };
+        }
+
+        public void InterpretationLoop()
+        {
+            while (true)
+            {
+
+            }
+        }
+
+        private List<Action<Chip8DataModel>> OpcodeInterpreter()
+        {
+            var opcodeFunctions = new List<Action<Chip8DataModel>>();
+
+            foreach (int opcode in this._rom)
+            {
+                switch ((opcode & 0xF000) >> 0xc)
+                {
+                    case 0x0:
+                        if ((opcode & 0x0F00) == 0)
+                        {
+                            switch (opcode & 0x000F)
+                            {
+                                case 0x0:
+                                    break;
+                                case 0xE:
+                                    break;
+                            }
+                        }
+                        else
+                        {
+                            opcodeFunctions.Add(OpcodeInstructions.Jump);
+                            //0nnn
+                        }
+                        break;
+                    case 0x1:
+                        break;
+                    case 0x2:
+                        break;
+                    case 0x3:
+                        break;
+                    case 0x4:
+                        break;
+                    case 0x5:
+                        break;
+                    case 0x6:
+                        break;
+                    case 0x7:
+                        break;
+                    case 0x8:
+                        break;
+                    case 0x9:
+                        break;
+                    case 0xA:
+                        break;
+                    case 0xB:
+                        break;
+                    case 0xC:
+                        break;
+                    case 0xD:
+                        break;
+                    case 0xE:
+                        break;
+                    case 0xF:
+                        break;
+                }
+            }
+
+            return opcodeFunctions;
+        }
+    }
+}
