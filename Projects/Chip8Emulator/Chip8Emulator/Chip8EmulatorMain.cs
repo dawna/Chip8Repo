165a97d7509169b32b76b0935a907324e72d593f
dawna <adamdawn29@gmail.com>
2016-07-13 22:11:51 -0500

    Checked in rep

---
+++ Projects/Chip8Emulator/Chip8Emulator/Chip8EmulatorMain.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8Emulator
+{
+    class Chip8EmulatorMain
+    {
+        public static int[] ReadRomFromFile(string filePath)
+        {
+            var romData = new int[ConstantValues.TOTAL_MEM_STORAGE];
+            byte[] byteArray = File.ReadAllBytes(filePath);
+
+            if (byteArray.Length > ConstantValues.TOTAL_MEM_STORAGE)
+            {
+                //Throw exception.
+            }
+
+            int counter = 0;
+            for (int i = 0; i < byteArray.Length - 1; i+= 2)
+            {
+                romData[counter] = (byteArray[i] << 8) | byteArray[i + 1];
+                counter++;
+            }
+
+            return romData;
+        }
+
+        static void Main(string[] args)
+        {
+            string filePath = @"c:\users\adam\documents\visual studio 2013\Projects\Chip8Emulator\Chip8Emulator\c8games\VERS";
+
+            //Retrieves ROM and stores it.
+            ReadRomFromFile(filePath);
+            //Execute Opcodes
+        }
+    }
+} 
---
+++ Projects/Chip8Emulator/Chip8Emulator/ConstantValues.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chip8Emulator
+{
+    public class ConstantValues
+    {
+        public static int TOTAL_MEM_STORAGE = 0xFFF - 0x200;
+    }
+}
